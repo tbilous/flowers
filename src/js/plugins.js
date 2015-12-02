399020ceea4b8d379816b85f82d3c29d01ad90b0
Taras Bilous <tbilous@yahoo.com>
2015-12-02 19:53:16 +0200

    feedback

--- src/js/plugins.js
+++ src/js/plugins.js
@@ -59,54 +59,6 @@ $(document).ready(function () {
 
     $('body').on('click', '.anchor', scrollIfAnchor);
 
-// SPY MENU
-// Cache selectors
-    var lastId,
-        topMenu = $("#navbar"),
-        topMenuHeight = topMenu.outerHeight() * 1.5,
-// All list items
-        menuItems = topMenu.find("a"),
-// Anchors corresponding to menu items
-        scrollItems = menuItems.map(function () {
-            var item = $($(this).attr("href"));
-            if (item.length) {
-                return item;
-            }
-        });
-
-// Bind click handler to menu items
-// so we can get a fancy scroll animation
-    menuItems.click(function (e) {
-        var href = $(this).attr("href"),
-            offsetTop = href === "#" ? 0 : $(href).offset().top - topMenuHeight + 1;
-        $('html, body').stop().animate({
-            scrollTop: offsetTop
-        }, 300);
-        e.preventDefault();
-    });
-
-// Bind to scroll
-    $(window).scroll(function () {
-        // Get container scroll position
-        var fromTop = $(this).scrollTop() + topMenuHeight;
-
-        // Get id of current scroll item
-        var cur = scrollItems.map(function () {
-            if ($(this).offset().top < fromTop)
-                return this;
-        });
-        // Get the id of the current element
-        cur = cur[cur.length - 1];
-        var id = cur && cur.length ? cur[0].id : "";
-
-        if (lastId !== id) {
-            lastId = id;
-            // Set/remove active class
-            menuItems
-                .parent().removeClass("active")
-                .end().filter("[href=#" + id + "]").parent().addClass("active");
-        }
-    });
 
     // CENTERED MODAL
     $(".start-modal").click(function () {
@@ -126,6 +78,7 @@ $(document).ready(function () {
             url: "mail.php", // mail script
             data: $(this).serialize()
         }).done(function () {
+            alert('send');
             $(this).find("input").val("");
             $('#' + formID).trigger("reset");
             $('#callbackModal').modal('show');
@@ -141,22 +94,7 @@ $(document).ready(function () {
         }
     });
 
-    // Lost count digit flow
-/*    function runDigitFlow() {
-        $('.lost-count').each(function (i, el) {
-            var targetValue = $(el).attr('data-value');
-            var curValue = 1;
-            //var curValue = parseInt($("[data-number]").html());
-            var intervalHandle = setInterval(function () {
-                var delta = Math.round(Math.max(Math.min((targetValue - curValue) / 7, 12), 1));
-                curValue = curValue + delta;
-                $(el).html(curValue);
-                if (curValue >= targetValue)
-                    clearInterval(intervalHandle);
-            }, 50);
-        });
-    }*/
-     //runDigitFlow()
+
     //Scroll MONITOR
     $('.s-monitor').each(function (i, element) {
 
