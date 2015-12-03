da1508696583654d626d84420f65cf865c96f49d
Taras Bilous <tbilous@yahoo.com>
2015-12-03 14:07:42 +0200

    mail.php, css manifest, favicons

--- src/mail.php
+++ src/mail.php
@@ -1,13 +1,14 @@
 <?php
 
 $recepient = "tbilous@gmail.com";
-$sitename = "SKMEI Clock";
+$sitename = "Flowers Iris";
 
 
-$comment = trim($_POST["comment"]);
+//$comment = trim($_POST["comment"]);
 $name = trim($_POST["name"]);
 $phone = trim($_POST["phone"]);
-$message = "Name: $name \nPhone: $phone \nComment: $comment";
+$manifest = trim($_POST["manifest"]);
+$message = "Name: $name \nPhone: $phone \nManifestt: $manifest";
 
 $pagetitle = "New request from \"$sitename\"";
 mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
